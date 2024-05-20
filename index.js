43b0056b7205ce4309d0468fcb84498f70a38e7e
Eliott McKenzie <e4991m@gmail.com>
2024-05-20 21:03:06 +0100

    restructured first middleware

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 import express from 'express'
 
-console.log('Hello World!')
+// console.log('Hello World!')
 const PORT = 5000
 
 const app = express()
 app.listen(PORT, () => console.log(`Express app is running on ${PORT}`))
 
-app.use((req, res, next) => {
-  console.log(req)
-})
+const logger = (req, res, next) => {
+  console.log(`incoming request on route: ${req.method} - ${req.url}`)
+  next()
+}
+
+app.use(logger)
 
 console.log({ app })
