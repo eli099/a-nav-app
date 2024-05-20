acff483f1e1b7fc64c3cdb0442412f9df6dd9876
Eliott McKenzie <e4991m@gmail.com>
2024-05-20 20:46:55 +0100

    added use for middleware and nodemon

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ const PORT = 5000
 const app = express()
 app.listen(PORT, () => console.log(`Express app is running on ${PORT}`))
 
+app.use((req, res, next) => {
+  console.log(req)
+})
+
 console.log({ app })
