11552006b8fbd1c06dfd445b367f9dfaab10ca07
Eliott McKenzie <e4991m@gmail.com>
2024-05-20 21:20:18 +0100

    parsed body and created enendpoint

--- index.js
+++ index.js
@@ -6,11 +6,28 @@ const PORT = 5000
 const app = express()
 app.listen(PORT, () => console.log(`Express app is running on ${PORT}`))
 
+// Mock Database
+const dogs = [
+  { name: 'Rodney', age: 12 },
+  { name: 'Dolly', age: 9 },
+  { name: 'Monty', age: 5 }
+]
+
 const logger = (req, res, next) => {
+  console.log('Request body', req.body)
   console.log(`incoming request on route: ${req.method} - ${req.url}`)
   next()
 }
 
 app.use(logger)
+
+// Parse body into JSON
+app.use(express.json())
+
+// Create endpoint
+app.use((req, res, next) => {
+  res.end('Welcome to our API!')
+})
+
 
 console.log({ app })
