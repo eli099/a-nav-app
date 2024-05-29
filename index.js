8a9cda7fe2baef0394520a36821f0a63f9da69bc
Eliott McKenzie <e4991m@gmail.com>
2024-05-29 23:01:33 +0100

    added logger

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ const PORT = 5000
 const app = express()
 
 
+// create logger middleware that will be used with within our request <-> response cycle by app.use
+// app.use is express function that gives access to request, response and next object
+
+const logger = (req, res, next) => {
+  console.log(`🚨 - Incoming request on ${req.method} - ${req.url}`)
+  next()
+}
+
+console.log(logger)
