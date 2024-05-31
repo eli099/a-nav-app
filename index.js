42568b39ed224d351718988c76a32c3dcbff2813
Eliott McKenzie <e4991m@gmail.com>
2024-05-31 19:24:28 +0100

    overwrite local file

--- index.js
+++ index.js
@@ -1,18 +1,57 @@
 import express from 'express'
 
-// import { v4 as uuid } from 'uuid'
+import { v4 as uuid } from 'uuid'
 
+// console.log('Hello World!')
 const PORT = 5000
 
 const app = express()
 
+// Create endpoint
+const handleRouteRequest = (req, res, next) => {
+  return res.end('Welcome to our root endpoint of the API')
+}
 
-// create logger middleware that will be used with within our request <-> response cycle by app.use
-// app.use is express function that gives access to request, response and next object
+const handleNotFound = (req, res, next) => {
+  return res.status(404).end('Not Found')
+}
+
+app.listen(PORT, () => console.log(`Express app is running on ${PORT}`))
+
+// Mock Database
+const users = [
+  { name: 'Rodney', location: 'UK' },
+  { name: 'Dolly', location: 'USA' },
+  { name: 'Monty', location: 'Japan' }
+]
 
 const logger = (req, res, next) => {
-  console.log(`🚨 - Incoming request on ${req.method} - ${req.url}`)
+  console.log('Request body', req.body)
+  console.log(`incoming request on route: ${req.method} - ${req.url}`)
   next()
 }
 
-console.log(logger)
+// Parse body into JSON
+app.use(express.json())
+
+app.use(logger)
+
+// Check get request is '/'
+app.get('/', handleRouteRequest)
+
+app.get('/users', (req, res, next) => {
+  return res.json(users)
+})
+
+// Add post request
+app.post('/user', (req, res, next) => {
+  const { body: newUser } = req
+  users.push(newUser)
+  console.log(users)
+  return res.end(`User with name ${req.body.name} has been added.`)
+})
+
+// If not return handleNotFound
+app.use(handleNotFound)
+
+console.log({ app })
